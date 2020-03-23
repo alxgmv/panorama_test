c9c3867fdf6f7a7e776c2a3b391a705915c49657
alxgmv <alxgmv@gmail.com>
2020-03-23 10:25:00 +0100

    1st

---
+++ data.js
@@ -0,0 +1,83 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-paris-showroom",
+      "name": "Paris showroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2773770509055744,
+          "pitch": -0.11402052733889789,
+          "rotation": 0,
+          "target": "1-123"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-123",
+      "name": "123",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0482496213736834,
+          "pitch": -0.01374889902044174,
+          "rotation": 0,
+          "target": "0-paris-showroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
